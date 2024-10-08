5ac47998cd3ca82a8091e108fc9d8845d8b96e1d
Titusz <titusz.boros@student.upt.ro>
2024-10-09 00:51:43 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5TQLJE2T.mjs";import"./chunk-67AFXVCK.mjs";import"./chunk-H3JLPOFK.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
+import{a}from"./chunk-XJKAE7H6.mjs";import"./chunk-67AFXVCK.mjs";import"./chunk-H3JLPOFK.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
